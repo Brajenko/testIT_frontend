0b6c5d55381ce2bb5c0ff8f5e70f7914e90fd535
Brajenko <goshabrajenko@gmail.com>
2024-01-30 21:56:27 +0300

    rewriting frontend using react.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Question from "./components/Question";
+import "./styles/App.css"
 
 function App() {
+  const questions = [
+    {
+      id: 0,
+      type: "text",
+      title: "Вопрос 1",
+      description: "Описание 1",
+      variants: []
+    },
+    {
+      id: 1,
+      type: "check",
+      title: "Вопрос 2",
+      description: "Описание 2",
+      variants: ["Вариант1", "Вариант2", "Вариант3", "Вариант4"]
+    },
+    {
+      id: 2,
+      type: "radio",
+      title: "Вопрос 3",
+      description: "Описание 3",
+      variants: ["Вариант1", "Вариант2", "Вариант3", "Вариант4"]
+    },
+    {
+      id: 3,
+      type: "code",
+      title: "Вопрос 4",
+      description: "Описание 4",
+      // variants: []
+    },
+  ];
+  const questionsToRender = [];
+  questions.forEach(function (question) {
+    questionsToRender.push(
+      <Question question={ question } key={ question.id }></Question>
+    )
+  })
+  const answers = [
+    {
+      id: 0,
+      answer: "sdfsdfsdf"
+    },
+    {
+      id: 1,
+      answer: "no answer",
+    },
+    {
+      id: 2,
+      answer: "no answer",
+    },
+    {
+      id: 3,
+      answer: "for x in range(123):\n\tprint(123)"
+    }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="questions">
+        { questionsToRender }
+      </div>
     </div>
   );
 }
